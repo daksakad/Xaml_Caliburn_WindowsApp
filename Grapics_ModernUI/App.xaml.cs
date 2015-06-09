0df61609278f5920e72af4b4e30e79523f5db8b6
daksakad <jdavidromo@live.com.mx>
2015-06-09 11:12:32 -0500

    -First Commit

---
+++ Grapics_ModernUI/App.xaml.cs
@@ -0,0 +1,84 @@
+﻿using Caliburn.Micro;
+using Grapics_ModernUI.ViewModels;
+using Grapics_ModernUI.Views;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.ApplicationModel;
+using Windows.ApplicationModel.Activation;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Application template is documented at http://go.microsoft.com/fwlink/?LinkId=234227
+
+namespace Grapics_ModernUI
+{
+    /// <summary>
+    /// Provides application-specific behavior to supplement the default Application class.
+    /// </summary>
+    public sealed partial class App
+    {
+        private WinRTContainer container;
+
+        /// <summary>
+        /// Initializes the singleton application object.  This is the first line of authored code
+        /// executed, and as such is the logical equivalent of main() or WinMain().
+        /// </summary>
+        public App()
+        {
+            this.InitializeComponent();
+        }
+
+
+
+        protected override void Configure()
+        {
+            container = new WinRTContainer();
+
+            container.RegisterWinRTServices();
+
+            container.PerRequest<GraphicsViewModel>();
+
+        }
+
+        protected override void PrepareViewFirst(Frame rootFrame)
+        {
+            container.RegisterNavigationService(rootFrame);
+        }
+
+        protected override void OnLaunched(LaunchActivatedEventArgs args)
+        {
+            DisplayRootView<GraphicsView>();
+
+        }
+
+        protected override object GetInstance(Type service, string key)
+        {
+            return container.GetInstance(service, key);
+        }
+
+        protected override IEnumerable<object> GetAllInstances(Type service)
+        {
+            return container.GetAllInstances(service);
+        }
+
+        protected override void BuildUp(object instance)
+        {
+            container.BuildUp(instance);
+        }
+
+        protected override void OnSuspending(object sender, Windows.ApplicationModel.SuspendingEventArgs e)
+        {
+            base.OnSuspending(sender, e);
+        }
+    }
+}
---
+++ Grapics_ModernUI/ViewModels/GraphicsViewModel.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Caliburn.Micro;
+
+namespace Grapics_ModernUI.ViewModels
+{
+    public class GraphicsViewModel : Screen
+    {
+        public GraphicsViewModel()
+        {
+            LoadChartContents();
+        }
+        private IObservableCollection<FinancialStuff> _financialStuffList;
+
+        public IObservableCollection<FinancialStuff> FinancialStuffList
+        {
+            get { return _financialStuffList; }
+            set
+            {
+                _financialStuffList = value;
+                NotifyOfPropertyChange(() => FinancialStuffList);
+            }
+        }
+
+        public class FinancialStuff
+        {
+            public string Name { get; set; }
+            public int Amount { get; set; }
+        }
+
+        private void LoadChartContents()
+        {
+            Random rand = new Random();
+            FinancialStuffList = new BindableCollection<FinancialStuff>
+            {
+                new FinancialStuff() {Name = "MSFT", Amount = rand.Next(0, 200)},
+                new FinancialStuff() {Name = "AAPL", Amount = rand.Next(0, 200)},
+                new FinancialStuff() {Name = "GOOG", Amount = rand.Next(0, 200)},
+                new FinancialStuff() {Name = "BBRY", Amount = rand.Next(0, 200)},
+            };
+        }
+    }
+}
